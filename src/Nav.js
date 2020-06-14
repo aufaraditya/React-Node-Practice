3e5e683b221fd539dac722af8ee983a47d5eb624
aufaraditya <adityaaufar@gmail.com>
2020-06-14 23:24:19 +0700

    FInal Commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useState,useContext} from 'react'
 import './Nav.css';
+import {MovieContext} from './MovieContext'
 
 export default function Nav() {
+    const [movies,setMovies] = useContext(MovieContext);
+
+
     return (
         <nav>
             <h3>Auf</h3>
-            <p>List of Movies:</p>
+            <p>List of Movies: {movies.length}</p>
         </nav>
     )
 }
